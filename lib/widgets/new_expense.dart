70828da233a8426204cb85c0e419e68671ffc1e6
ChetanVarunKanipakam <chetanvarun131@gmail.com>
2025-05-28 22:47:41 +0530

    updated modal

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,6 +10,24 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController=TextEditingController();
+  final _amountController=TextEditingController();
+
+
+  void _showDatePicker(){
+    final initialDate=DateTime.now();
+    final firstDate=DateTime(initialDate.year-1,initialDate.month,initialDate.day);
+    final lastDate=DateTime.now();
+    showDatePicker(context: context, initialDate: initialDate,firstDate: firstDate, lastDate: lastDate);
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -17,11 +35,42 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
             decoration: InputDecoration(
               label: Text("Title")
             ),
           ),
+          Row(children: [
+            Expanded(child: TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              prefixText: "\$ ",
+              label: Text("Amount")
+            ),
+          )),
+          const SizedBox(width: 16,),
+          Expanded(child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+            Text("Select Date"),
+            IconButton(onPressed: _showDatePicker,
+             icon:const  Icon(Icons.calendar_month))
+          ],))
+          ],),
+          
+          Row(children: [
+            TextButton(onPressed: (){
+              Navigator.pop(context);
+            },
+             child: Text("Cancel"),
+             ),
+            ElevatedButton(onPressed: (){
+              print(_titleController.text);
+            }, child: Text("Save Expense"),
+            ),
+          ],)
           // Expanded(child: Row(children: [
           //   TextField(
           //     decoration: InputDecoration(
